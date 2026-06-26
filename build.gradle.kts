49d57f2ade76fbcc65d9d55a666c987d5a09a402
coderxi <coderxi@qq.com>
2026-06-26 15:51:56 +0800

    chore(dept): update paper-userdev version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.3.0" apply false
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.19" apply false
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.21" apply false
     base
 }
 
