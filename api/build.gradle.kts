903935f22f5d0800b2ecbc62a45f85e6dcea0074
coderxi <coderxi@qq.com>
2026-06-27 00:53:39 +0800

    ci: add build and release workflow

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,3 +1,29 @@
+plugins {
+    id("maven-publish")
+}
+
 dependencies {
     compileOnly("io.papermc.paper:paper-api:${project.findProperty("paper-api.version.base")}")
+}
+
+java {
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/xiplugin/FakePlayerPlus")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
