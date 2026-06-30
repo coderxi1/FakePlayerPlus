4f9e19663f331ff58bcd98aca8eabc7b991f10ad
coderxi <coderxi@qq.com>
2026-07-01 03:10:13 +0800

    fix: fake players unable to be kicked in 26.1.1+ by using `/kick` or `/fp remove`

--- nms/nms_v26_1_1/src/main/kotlin/com/coderxi/plugin/fakeplayer/nms/v26_1_1/NMSServerGamePacketListenerImpl.kt
+++ nms/nms_v26_1_1/src/main/kotlin/com/coderxi/plugin/fakeplayer/nms/v26_1_1/NMSServerGamePacketListenerImpl.kt
@@ -1,11 +1,20 @@
 package com.coderxi.plugin.fakeplayer.nms.v26_1_1
 
-import com.coderxi.plugin.fakeplayer.api.FakePlayerPlusPluginApi.Companion.javaPlugin as plugin
 import com.coderxi.plugin.fakeplayer.nms.v1_21_11.network.FakeConnection
+import io.papermc.paper.adventure.PaperAdventure
+import io.papermc.paper.configuration.GlobalConfiguration
+import io.papermc.paper.connection.DisconnectionReason
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.ComponentLike
+import net.kyori.adventure.text.format.NamedTextColor
+import net.minecraft.network.DisconnectionDetails
 import net.minecraft.network.protocol.game.ClientboundSetEntityMotionPacket
 import net.minecraft.server.dedicated.DedicatedServer
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.server.network.CommonListenerCookie
+import org.bukkit.event.player.PlayerKickEvent
+import org.bukkit.event.player.PlayerQuitEvent
+import com.coderxi.plugin.fakeplayer.api.FakePlayerPlusPluginApi.Companion.javaPlugin as plugin
 
 open class NMSServerGamePacketListenerImpl(
     server: DedicatedServer,
@@ -17,6 +26,32 @@ open class NMSServerGamePacketListenerImpl(
     override fun handleClientboundSetEntityMotionPacket(packet: ClientboundSetEntityMotionPacket) {
         if (packet.id() != player.id) return
         player.bukkitEntity.scheduler.execute(plugin,{ player.lerpMotion(packet.movement) },null,1L)
+    }
+
+    @Suppress("UnstableApiUsage")
+    override fun disconnect(details: DisconnectionDetails) {
+        if (!this.cserver.isPrimaryThread) {
+            super.disconnect(details)
+            return
+        }
+        val cause = (details.disconnectionReason().orElseThrow() as DisconnectionReason).game().orElse(PlayerKickEvent.Cause.UNKNOWN) as PlayerKickEvent.Cause
+        val rawLeaveMessage: Component = Component.translatable(
+            "multiplayer.player.left",
+            NamedTextColor.YELLOW,
+            *arrayOf((if (GlobalConfiguration.get().messages.useDisplayNameInQuitMessage) handle.bukkitEntity.displayName() else Component.text(handle.scoreboardName)) as ComponentLike)
+        )
+        val event = PlayerKickEvent(player.bukkitEntity, PaperAdventure.asAdventure(details.reason()),
+            rawLeaveMessage,
+            cause
+        )
+        if (this.cserver.server.isRunning) {
+            this.cserver.pluginManager.callEvent(event)
+        }
+        if (event.isCancelled) {
+            return
+        }
+        handle.quitReason = PlayerQuitEvent.QuitReason.KICKED
+        server.playerList.remove(handle)
     }
 
 }
--- plugin/src/main/kotlin/com/coderxi/plugin/fakeplayer/utils/NMSBridgeLoader.kt
+++ plugin/src/main/kotlin/com/coderxi/plugin/fakeplayer/utils/NMSBridgeLoader.kt
@@ -6,16 +6,13 @@ object NMSBridgeLoader : PluginComponent {
 
     // https://docs.papermc.io/paper/dev/internals/#getting-the-current-minecraft-version
     fun load(minecraftVersion: String): NMSBridge {
-        val (mainVersion,v2) = (0..2).map { minecraftVersion.split(".").getOrNull(it)?.toIntOrNull() ?: 0 }
+        val (mainVersion,v2,v3) = (0..2).map { minecraftVersion.split(".").getOrNull(it)?.toIntOrNull() ?: 0 }
         return when {
             minecraftVersion == "1.21.11" -> {
                 com.coderxi.plugin.fakeplayer.nms.v1_21_11.NMSBridgeImpl()
             }
-            mainVersion == 26 && v2 == 1 -> {
-                com.coderxi.plugin.fakeplayer.nms.v26_1_1.NMSBridgeImpl()
-            }
             mainVersion >= 26 -> {
-                com.coderxi.plugin.fakeplayer.nms.v26_2.NMSBridgeImpl()
+                com.coderxi.plugin.fakeplayer.nms.v26_1_1.NMSBridgeImpl()
             }
             else -> {
                 throw Exception("Running on an unsupported version ($minecraftVersion).")
